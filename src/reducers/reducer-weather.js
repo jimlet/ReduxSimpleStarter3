a87f8e94ecd9945e491ce70296003ea272243043
jimlet <jim@coutu.org>
2018-08-22 22:10:12 -0500

    finished

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,3 +1,18 @@
-export default function(state = null, action) {
+import { actionTypes } from '../actions/index';
+
+export default function(state = [], action) {
+  // console.log(action);
+
+  switch (action.type) {
+    case actionTypes.FETCH_WEATHER:
+      // NEVER do this because it mutates state.
+      // state.push(action.payload.data);
+
+      // Make a new array and return that as state
+      // This is ok, but it's not as cool as the spread operator:
+      // return state.concat([action.payload.data]);
+      return [action.payload.data, ...state];
+  }
+  
   return state;
 }
